887c7fa7c8f4a0346b77fbbf37746f9921023cc2
Aklimabegum1010 <150171856+Aklimabegum1010@users.noreply.github.com>
2026-02-06 20:26:39 +0600

    update some project files

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const api_base = "https://openapi.programming-hero.com/api/phero-tube"
 
+const displayCategories = (categories = []) => {
+    const categoryContainer = document.getElementById('category-container')
+    categories.forEach(category => {
+
+        const button = document.createElement('button')
+
+        button.id = `btn-${category.category_id}`
+
+        button.className = "px-6 py-2.5 bg-[#151520] border border-[#2a2a3e] text-gray-300 font-medium rounded-full whitespace-nowrap hover:border-[#ff1f3d] hover:text-white hover:-translate-y-0.5 transition-all duration-300";
+
+
+        // button.className = "px-6 py-3 bg-gradient-primary text-white font-semibold rounded-full shadow-lg shadow-[#ff1f3d]/30 hover:shadow-[#ff1f3d]/40 hover:-translate-y-0.5 transition-all duration-300"
+
+
+
+
+        button.textContent = category.category
+        categoryContainer.appendChild(button)
+        // console.log(button)
+
+    })
+}
 
 const loadCategories = async () => {
     try {
         const response = await fetch(`${api_base}/categories`)
         const data = await response.json()
-        console.log(data)
+        const { categories } = data
+        // console.log(categories)
+        displayCategories(categories)
     }
     catch (err) {
         console.error('error loading categories', err)
@@ -14,12 +38,25 @@ const loadCategories = async () => {
 
 }
 
-
 const initialApp = async () => {
     await loadCategories()
 }
 
 initialApp()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
